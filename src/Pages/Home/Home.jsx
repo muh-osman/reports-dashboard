445ac72983ac9ee8b96ef906bddd93cfe0d5e8f2
muh-osman <mr.muhammed.osman@gmail.com>
2024-10-31 14:17:03 +0300

    Fix

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,19 @@
 import style from "./Home.module.scss";
+import { useEffect } from "react";
 // MUI
 import Divider from "@mui/material/Divider";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export default function Home() {
+  useEffect(() => {
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div dir="rtl" className={style.container}>
       <div className={style.points_container}>
